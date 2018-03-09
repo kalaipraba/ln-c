784c5d9ca4ef12b6e435e3d4025327cc133a9b33
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-03-09 16:17:19 +0530

    MODULO.C

---
+++ MODULO.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float num1, num2, rem;
+ 
+  printf("First Number?\n");
+  scanf("%f%",&num1);
+  printf("Second Number?\n");
+  scanf("%f",&num2);
+ 
+  rem = fmod(num1,num2);
+ 
+  printf("%.2f divided by %.2f is remainder %.2f\n", num1, num2, rem);
+ 
+ 
+  getchar();
+  return 0;
+}
